699c9e6ad4a557fdaef7b5e947e60b94a62a02ae
Scott Williams <scott93@gmail.com>
2017-11-04 21:53:41 -0400

    Formatting

--- test/versioncheckr.test.js
+++ test/versioncheckr.test.js
@@ -1,41 +1,55 @@
 'use strict';
 
 const crypto = require('crypto'),
-      proxyquire =  require('proxyquire'),
-      expect = require( 'chai' ).expect;
+  proxyquire = require('proxyquire'),
+  expect = require('chai').expect;
 
 class S3 {
-  constructor(){
+  constructor() {
     this.getObject = () => ({
-      promise: () => Promise.resolve({Body: 'cert'})
+      promise: () => Promise.resolve({
+        Body: 'cert'
+      })
     });
   }
 }
 
 class GitHubApi {
-  constructor(){
+  constructor() {
     this.authenticate = () => {};
     this.apps = {
-      createInstallationToken: () => Promise.resolve({data: {token: "1"}})
+      createInstallationToken: () => Promise.resolve({
+        data: {
+          token: "1"
+        }
+      })
     };
     this.repos = {
       getContent: (params) => Promise.resolve({
-          data: {
-            content: new Buffer(`{"version": "${params.ref }"}`).toString('base64')
-          }
-        }),
-      createStatus: (status) => Promise.resolve({data: {description: status.description}})
+        data: {
+          content: new Buffer(`{"version": "${params.ref }"}`).toString('base64')
+        }
+      }),
+      createStatus: (status) => Promise.resolve({
+        data: {
+          description: status.description
+        }
+      })
     };
- }
+  }
 }
 
 const myLambda = proxyquire('../versioncheckr', {
-  'aws-sdk': { S3 },
+  'aws-sdk': {
+    S3
+  },
   'github': GitHubApi,
-  'jsonwebtoken': { sign: () => {} }
+  'jsonwebtoken': {
+    sign: () => {}
+  }
 });
 
-function createHash(secret, body){
+function createHash(secret, body) {
   const hmac = crypto.createHmac('sha1', secret);
   hmac.setEncoding('hex');
   hmac.write(body);
@@ -80,194 +94,187 @@ beforeEach(() => {
   process.env.WEBHOOK_SECRET = 'password';
 });
 
-describe( 'versioncheckr', () => {
+//since versioncheckr uses promises that aren't returned to the caller
+//https://github.com/mochajs/mocha/issues/2797
+process.on('unhandledRejection', e => {
+  throw e;
+});
 
-  it( `Missing X-GitHub-Event`, ( done ) => {
-    const gitHubEvent = makeEvent('action','event');
+describe('versioncheckr', () => {
+
+  it(`Missing X-GitHub-Event`, (done) => {
+    const gitHubEvent = makeEvent('action', 'event');
     delete gitHubEvent.headers['X-GitHub-Event'];
 
-    myLambda.handler( gitHubEvent, {}, (err, result) => {
-      try {
-        expect( err ).to.not.exist;
-        expect( result ).to.exist;
-        expect( result.statusCode ).to.equal(400);
-        expect( result.body ).to.equal('Missing X-GitHub-Event');
-        done();
-      }
-      catch( error ) {
-          done( error );
-      }
+    myLambda.handler(gitHubEvent, {}, (err, result) => {
+      expect(err).to.not.exist;
+      expect(result).to.exist;
+      expect(result.statusCode).to.equal(400);
+      expect(result.body).to.equal('Missing X-GitHub-Event');
+      done();
     });
   });
 
-  it( `Missing X-Hub-Signature`, ( done ) => {
-    const gitHubEvent = makeEvent('action','event');
+  it(`Missing X-Hub-Signature`, (done) => {
+    const gitHubEvent = makeEvent('action', 'event');
     delete gitHubEvent.headers['X-Hub-Signature'];
 
-    myLambda.handler( gitHubEvent, {}, (err, result) => {
-      try {
-        expect( err ).to.not.exist;
-        expect( result ).to.exist;
-        expect( result.statusCode ).to.equal(400);
-        expect( result.body ).to.equal('Missing X-Hub-Signature');
-        done();
-      }
-      catch( error ) {
-          done( error );
-      }
+    myLambda.handler(gitHubEvent, {}, (err, result) => {
+      expect(err).to.not.exist;
+      expect(result).to.exist;
+      expect(result.statusCode).to.equal(400);
+      expect(result.body).to.equal('Missing X-Hub-Signature');
+      done();
     });
   });
 
-  it( `Invalid X-Hub-Signature`, ( done ) => {
-    const gitHubEvent = makeEvent('action','event');
+  it(`Invalid X-Hub-Signature`, (done) => {
+    const gitHubEvent = makeEvent('action', 'event');
     const body = '{ "foo": "bar" }';
     const differentBody = '{ "bar": "foo" }';
     const hash = createHash('not_the_secret', body);
     gitHubEvent.body = differentBody;
     gitHubEvent.headers['X-Hub-Signature'] = `sha1=${hash}`;
 
-    myLambda.handler( gitHubEvent, {}, (err, result) => {
-      try {
-        expect( err ).to.not.exist;
-        expect( result ).to.exist;
-        expect( result.statusCode ).to.equal(400);
-        expect( result.body ).to.equal('Invalid X-Hub-Signature');
-        done();
-      }
-      catch( error ) {
-          done( error );
-      }
+    myLambda.handler(gitHubEvent, {}, (err, result) => {
+      expect(err).to.not.exist;
+      expect(result).to.exist;
+      expect(result.statusCode).to.equal(400);
+      expect(result.body).to.equal('Invalid X-Hub-Signature');
+      done();
     });
   });
 
-  it( `Invalid secret for X-Hub-Signature`, ( done ) => {
-    const gitHubEvent = makeEvent('action','event');
+  it(`Invalid secret for X-Hub-Signature`, (done) => {
+    const gitHubEvent = makeEvent('action', 'event');
     const body = '{ "foo": "bar" }';
     const hash = createHash('not_the_secret', body);
     gitHubEvent.body = body;
     gitHubEvent.headers['X-Hub-Signature'] = `sha1=${hash}`;
 
-    myLambda.handler( gitHubEvent, {}, (err, result) => {
-      try {
-        expect( err ).to.not.exist;
-        expect( result ).to.exist;
-        expect( result.statusCode ).to.equal(400);
-        expect( result.body ).to.equal('Invalid X-Hub-Signature');
-        done();
-      }
-      catch( error ) {
-          done( error );
-      }
+    myLambda.handler(gitHubEvent, {}, (err, result) => {
+      expect(err).to.not.exist;
+      expect(result).to.exist;
+      expect(result.statusCode).to.equal(400);
+      expect(result.body).to.equal('Invalid X-Hub-Signature');
+      done();
     });
   });
 
-  it( `Valid X-Hub-Signature`, ( done ) => {
-    const gitHubEvent = makeEvent('action','event');
+  it(`Valid X-Hub-Signature`, (done) => {
+    const gitHubEvent = makeEvent('action', 'event');
     const body = '{ "foo": "bar" }';
     const hash = createHash(process.env.WEBHOOK_SECRET, body);
     gitHubEvent.body = body;
     gitHubEvent.headers['X-Hub-Signature'] = `sha1=${hash}`;
 
-    myLambda.handler( gitHubEvent, {}, (err, result) => {
-      try {
-        expect( err ).to.not.exist;
-        expect( result ).to.exist;
-        expect( result.statusCode ).to.not.equal(400);
-        expect( result.body ).to.not.equal('Invalid X-Hub-Signature');
-        done();
-      }
-      catch( error ) {
-          done( error );
-      }
+    myLambda.handler(gitHubEvent, {}, (err, result) => {
+      expect(err).to.not.exist;
+      expect(result).to.exist;
+      expect(result.statusCode).to.not.equal(400);
+      expect(result.body).to.not.equal('Invalid X-Hub-Signature');
+      done();
     });
   });
 
   [
-    {event: 'pull_request', action: 'create'},
-    {event: 'pull_request', action: 'fork'},
-    {event: 'release', action: 'published'},
-    {event: 'organization', action: 'member_added'}
-  ].forEach( (data) => {
-      it( `Ignore event=${data.event} with action=${data.action}`, ( done ) => {
-        myLambda.handler( makeEvent(data.action, data.event), {}, (err, result) => {
-          try {
-            expect( err ).to.not.exist;
-            expect( result ).to.exist;
-            expect( result.statusCode ).to.equal(202);
-            expect( result.body ).to.equal('No action to take');
-            done();
-          }
-          catch( error ) {
-              done( error );
-          }
-        });
+    {
+      event: 'pull_request',
+      action: 'create'
+    },
+    {
+      event: 'pull_request',
+      action: 'fork'
+    },
+    {
+      event: 'release',
+      action: 'published'
+    },
+    {
+      event: 'organization',
+      action: 'member_added'
+    }
+  ].forEach((data) => {
+    it(`Ignore event=${data.event} with action=${data.action}`, (done) => {
+      myLambda.handler(makeEvent(data.action, data.event), {}, (err, result) => {
+        expect(err).to.not.exist;
+        expect(result).to.exist;
+        expect(result.statusCode).to.equal(202);
+        expect(result.body).to.equal('No action to take');
+        done();
       });
+    });
   });
 
   [
     'opened',
     'reopened',
     'synchronize'
-  ].forEach( (gitHubAction) => {
-      it( `Process pull request with action: type=${gitHubAction}`, ( done ) => {
-        myLambda.handler( makeEvent(gitHubAction, 'pull_request'), {}, (err, result) => {
-          try {
-            expect( err ).to.not.exist;
-            expect( result ).to.exist;
-            expect( result.statusCode ).to.equal(200);
-            done();
-          }
-          catch( error ) {
-              done( error );
-          }
-        });
+  ].forEach((gitHubAction) => {
+    it(`Process pull request with action: type=${gitHubAction}`, (done) => {
+      myLambda.handler(makeEvent(gitHubAction, 'pull_request'), {}, (err, result) => {
+        expect(err).to.not.exist;
+        expect(result).to.exist;
+        expect(result.statusCode).to.equal(200);
+        done();
       });
+    });
   });
 
   [
-    {oldVersion: '1.0.0', newVersion: '1.0.1'},
-    {oldVersion: '0.0.0', newVersion: '1.0.0'},
-    {oldVersion: '1.0.0', newVersion: '1.1.0'},
-    {oldVersion: '9.9.99', newVersion: '10.0.0'}
-  ].forEach( (data) => {
-      it( `Version has been bumped: ${data.oldVersion} to ${data.newVersion}`, ( done ) => {
-        const event = makeEvent('opened', 'pull_request', data.oldVersion, data.newVersion);
-        myLambda.handler( event, {}, (err, result) => {
-          try {
-            expect( err ).to.not.exist;
-            expect( result ).to.exist;
-            expect( result.statusCode ).to.equal(200);
-            expect( result.body ).to.equal(`Version ${data.newVersion} will replace ${data.oldVersion}`);
-            done();
-          }
-          catch( error ) {
-              done( error );
-          }
-        });
+    {
+      oldVersion: '1.0.0',
+      newVersion: '1.0.1',
+      isVersionHigher: true
+    },
+    {
+      oldVersion: '0.0.0',
+      newVersion: '1.0.0',
+      isVersionHigher: true
+    },
+    {
+      oldVersion: '1.0.0',
+      newVersion: '1.1.0',
+      isVersionHigher: true
+    },
+    {
+      oldVersion: '9.9.99',
+      newVersion: '10.0.0',
+      isVersionHigher: true
+    },
+    {
+      oldVersion: '1.0.0',
+      newVersion: '1.0.0',
+      isVersionHigher: false
+    },
+    {
+      oldVersion: '1.0.0',
+      newVersion: '0.0.0',
+      isVersionHigher: false
+    },
+    {
+      oldVersion: '1.0.1',
+      newVersion: '1.0.0',
+      isVersionHigher: false
+    },
+    {
+      oldVersion: '1.1.0',
+      newVersion: '1.0.9',
+      isVersionHigher: false
+    }
+  ].forEach((data) => {
+    const msg = data.isVersionHigher ?
+      `Version ${data.newVersion} will replace ${data.oldVersion}` : `Version ${data.newVersion} should be bumped greater than ${data.oldVersion}`;
+    it(msg, (done) => {
+      const event = makeEvent('opened', 'pull_request', data.oldVersion, data.newVersion);
+      myLambda.handler(event, {}, (err, result) => {
+        expect(err).to.not.exist;
+        expect(result).to.exist;
+        expect(result.statusCode).to.equal(200);
+        expect(result.body).to.equal(msg);
+        done();
       });
-  });
-
-  [
-    {oldVersion: '1.0.0', newVersion: '1.0.0'},
-    {oldVersion: '1.0.0', newVersion: '0.0.0'},
-    {oldVersion: '1.0.1', newVersion: '1.0.0'},
-    {oldVersion: '1.1.0', newVersion: '1.0.9'}
-  ].forEach( (data) => {
-      it( `Version not bumped: ${data.oldVersion} to ${data.newVersion}`, ( done ) => {
-        const event = makeEvent('opened', 'pull_request', data.oldVersion, data.newVersion);
-        myLambda.handler( event, {}, (err, result) => {
-          try {
-            expect( err ).to.not.exist;
-            expect( result ).to.exist;
-            expect( result.statusCode ).to.equal(200);
-            expect( result.body ).to.equal(`Version ${data.newVersion} should be bumped greater than ${data.oldVersion}`);
-            done();
-          }
-          catch( error ) {
-              done( error );
-          }
-        });
-      });
+    });
   });
 
 });
--- versioncheckr.js
+++ versioncheckr.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const GitHubApi = require('github'),
-      AWS = require('aws-sdk'),
-      jwt = require('jsonwebtoken'),
-      semver = require('semver'),
-      crypto = require('crypto');
+  AWS = require('aws-sdk'),
+  jwt = require('jsonwebtoken'),
+  semver = require('semver'),
+  crypto = require('crypto');
 
 function validateSignature(body, xHubSignature) {
   const hmac = crypto.createHmac('sha1', process.env.WEBHOOK_SECRET);
@@ -16,14 +16,14 @@ function validateSignature(body, xHubSignature) {
 }
 
 const privateKey = (() =>
-    new AWS.S3().getObject({
-      Bucket: process.env.PEM_BUCKET_NAME,
-      Key: process.env.PEM_KEY
-    }).promise()
-      .then( file => file.Body.toString('utf8') )
+  new AWS.S3().getObject({
+    Bucket: process.env.PEM_BUCKET_NAME,
+    Key: process.env.PEM_KEY
+  }).promise()
+  .then(file => file.Body.toString('utf8'))
 )();
 
-function gitHubAuthenticate (appId, cert, installationId) {
+function gitHubAuthenticate(appId, cert, installationId) {
   const github = new GitHubApi();
   const payload = {
     iat: Math.floor(new Date() / 1000),
@@ -32,10 +32,14 @@ function gitHubAuthenticate (appId, cert, installationId) {
   };
   github.authenticate({
     type: 'integration',
-    token: jwt.sign(payload, cert, {algorithm: 'RS256'})
+    token: jwt.sign(payload, cert, {
+      algorithm: 'RS256'
+    })
   });
 
-  return github.apps.createInstallationToken({installation_id: installationId})
+  return github.apps.createInstallationToken({
+      installation_id: installationId
+    })
     .then(res => {
       github.authenticate({
         type: 'token',
@@ -43,10 +47,12 @@ function gitHubAuthenticate (appId, cert, installationId) {
       });
       return github;
     })
-    .catch(err => { throw new Error(JSON.stringify(err)); });
+    .catch(err => {
+      throw new Error(JSON.stringify(err));
+    });
 }
 
-function getFilesFromGitHub (github, owner, repo, headRef, baseRef) {
+function getFilesFromGitHub(github, owner, repo, headRef, baseRef) {
   const getContentParams = {
     owner: owner,
     repo: repo,
@@ -60,62 +66,63 @@ function getFilesFromGitHub (github, owner, repo, headRef, baseRef) {
 
   return Promise.all([basePackageJson, headPackageJson])
     .then(([baseFile, headFile]) => ({
-        github: github,
-        oldVersion: JSON.parse(new Buffer(baseFile.data.content, 'base64')).version,
-        newVersion: JSON.parse(new Buffer(headFile.data.content, 'base64')).version
+      github: github,
+      oldVersion: JSON.parse(new Buffer(baseFile.data.content, 'base64')).version,
+      newVersion: JSON.parse(new Buffer(headFile.data.content, 'base64')).version
     }))
-    .catch(err => { throw new Error(JSON.stringify(err)); });
+    .catch(err => {
+      throw new Error(JSON.stringify(err));
+    });
 }
 
-function postStatus (github, owner, repo, sha, oldVersion, newVersion) {
+function postStatus(github, owner, repo, sha, oldVersion, newVersion) {
   const isNewer = semver.gt(newVersion, oldVersion);
+  const description = isNewer ?
+    `Version ${newVersion} will replace ${oldVersion}` : `Version ${newVersion} should be bumped greater than ${oldVersion}`;
 
   return github.repos.createStatus({
       owner: owner,
       repo: repo,
       sha: sha,
       state: isNewer ? 'success' : 'failure',
-      description: isNewer ? `Version ${newVersion} will replace ${oldVersion}` : `Version ${newVersion} should be bumped greater than ${oldVersion}`,
+      description: description,
       context: 'Version Checkr'
     })
-    .catch(err => { throw new Error(JSON.stringify(err)); });
+    .catch(err => {
+      throw new Error(JSON.stringify(err));
+    });
+}
+
+function createResponse(statusCode, msg) {
+  return {
+    statusCode: statusCode,
+    headers: {
+      'Content-Type': 'text/plain'
+    },
+    body: msg
+  };
 }
 
 module.exports.handler = (event, context, callback) => {
 
   const githubEvent = event.headers['X-GitHub-Event'];
   if (!githubEvent) {
-    return callback( null, {
-        statusCode: 400,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Missing X-GitHub-Event'
-      });
+    return callback(null, createResponse(400, 'Missing X-GitHub-Event'));
   }
 
-  var sig = event.headers['X-Hub-Signature'];
+  const sig = event.headers['X-Hub-Signature'];
   if (!sig) {
-    return callback( null, {
-        statusCode: 400,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Missing X-Hub-Signature'
-      });
+    return callback(null, createResponse(400, 'Missing X-Hub-Signature'));
   }
   if (!validateSignature(event.body, sig)) {
-    return callback( null, {
-        statusCode: 400,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Invalid X-Hub-Signature'
-      });
+    return callback(null, createResponse(400, 'Invalid X-Hub-Signature'));
   }
 
   const pullRequest = JSON.parse(event.body);
 
-  if (githubEvent !== 'pull_request' || !(pullRequest.action === 'opened' || pullRequest.action === 'reopened' || pullRequest.action === 'synchronize')) {
-    return callback( null, {
-        statusCode: 202,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'No action to take'
-      });
+  if (githubEvent !== 'pull_request' ||
+    !(pullRequest.action === 'opened' || pullRequest.action === 'reopened' || pullRequest.action === 'synchronize')) {
+    return callback(null, createResponse(202, 'No action to take'));
   }
 
   const installationId = pullRequest.installation.id;
@@ -125,14 +132,10 @@ module.exports.handler = (event, context, callback) => {
   const sha = pullRequest.pull_request.head.sha;
   const baseRef = pullRequest.pull_request.base.ref;
 
-  Promise.resolve( privateKey )
-    .then( privateKey => gitHubAuthenticate(process.env.APP_ID, privateKey, installationId) )
-    .then( github => getFilesFromGitHub(github, owner, repo, headRef, baseRef) )
-    .then( res => postStatus(res.github, owner, repo, sha, res.oldVersion, res.newVersion) )
-    .then( res => callback( null, {
-        statusCode: 200,
-        headers: { 'Content-Type': 'text/plain' },
-        body: res.data.description
-      }))
-    .catch( err => { callback(err); } );
+  Promise.resolve(privateKey)
+    .then(privateKey => gitHubAuthenticate(process.env.APP_ID, privateKey, installationId))
+    .then(github => getFilesFromGitHub(github, owner, repo, headRef, baseRef))
+    .then(res => postStatus(res.github, owner, repo, sha, res.oldVersion, res.newVersion))
+    .then(res => callback(null, createResponse(200, res.data.description)))
+    .catch(err => callback(err));
 };
